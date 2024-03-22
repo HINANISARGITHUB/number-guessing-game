577e75bc63238817a9f222ffd2b6b1c706fd5589
HinaNisar <160288150+HINANISARGITHUB@users.noreply.github.com>
2024-03-23 03:52:06 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: (chalk.greenBright("Please guessed a number between 1-6:")),
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log(chalk.cyanBright("Congratulation! you guessed right number"));
+}
+else {
+    console.log(chalk.redBright("You guessed wrong number"));
+}
